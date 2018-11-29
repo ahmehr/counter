704097391da8326dfc4ab8a99bbbc64282370aab
ahmehr <ah.mehr@yahoo.com>
2018-11-29 12:35:26 -0800

    include all changes

---
+++ CounterApi/Controllers/CounterController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CounterApi.Controllers
+{
+    //* Url + Route = Address for Calling Api *\\
+    [Route("api")]
+    //* Prevent Caching on Server Side To retriving new value for Global and Static Values *\\
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //* Autorization to EveryOne, Eny Orgin By Enabling Cors Policy Defined in StartUp Class *\\
+    [EnableCors("CorsPolicy")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        //* Global Variable Stored In Memory *\\
+        public static class Globals
+        {
+            //* parameterless constructor required for static class *\\
+            static Globals() { Counter = 0; } //* default value *\\
+
+            //* public get, and private set for strict access control *\\
+            public static int Counter { get; set; }
+
+            //* GlobalInt can be changed only via this method *\\
+            public static void SetGlobalInt(int newInt)
+            {
+                Counter = newInt;
+            }
+        }
+        //* Get Method Retrive The Counter *\\
+        [HttpGet]
+        public int Get()
+        {
+            return Globals.Counter;
+        }
+        //* Put Method Incerement The Counter by Requqsted Rate in Query String in Url *\\
+        [HttpPut]
+        public string Put([FromQuery] int inc)   
+        {
+            Globals.SetGlobalInt( Globals.Counter + inc);
+            return Globals.Counter.ToString();
+        }
+        //* Post Method Reset The Counter *\\
+        [HttpPost]
+        public void Reset()
+        {
+            Globals.SetGlobalInt(0);
+        }
+    }
+}
---
+++ CounterApi/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CounterApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //* Adding cors to Configuration and Defining Policy with Appropriate Parameter *\\
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin()
+                   );
+            });  
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            //* Adding cors Policy to app *\\
+            app.UseCors("CorsPolicy");
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }     
+    }
+}
